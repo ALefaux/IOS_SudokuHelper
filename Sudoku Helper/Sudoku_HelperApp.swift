e7f46f91437097e6e616eac83e46210a0aa474bb
Axel Lefaux <axel.lefaux@outlook.fr>
2020-12-05 11:54:01 +0100

    Initial Commit

---
+++ Sudoku Helper/Sudoku_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_HelperApp.swift
+//  Sudoku Helper
+//
+//  Created by Axel Lefaux on 05/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
